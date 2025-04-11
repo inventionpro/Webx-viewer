149c5a5c3056bd13839aab4f4eb2483c8704d80a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-11 16:13:54 +0200

    Fix

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import parse as htmlparser from './parsers/html.js';
-//import parse as cssparser from './parsers/css.js';
+import { parse as htmlparser } from './parsers/html.js';
+//import { parse as cssparser } from './parsers/css.js';
 
-import build as htmlbuilder from './builder/html.js';
-//import build as cssbuilder from './builder/css.js';
+import { build as htmlbuilder } from './builder/html.js';
+//import { build as cssbuilder } from './builder/css.js';
 
 function bussFetch(ip, path) {
   if (ip.includes('github.com')) ip = ip.replace('github.com','raw.githubusercontent.com')+'/main/'+path;
