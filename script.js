34ee18a6831a136bcaffaa33d69bf347d25941a4
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-12 19:19:10 +0200

    More compatible lua

--- script.js
+++ script.js
@@ -19,21 +19,105 @@ function bussFetch(ip, path) {
 
 function HTMLElementFunctionsFor(elem) {
   let tag = elem.tagName.toLowerCase();
+  let bussinga = document.getElementById('bussinga').checked;
   let base = {
-    get_contents: () => elem.textContent,
-    set_contents: (text) => elem.textContent = text,
+    get_contents: () => elem.value || elem.checked || elem.textContent,
+    get_href: () => elem.href,
+    get_source: () => elem.src,
+    get_opacity: () => elem.style.opacity,
 
-    on_click: (luaCallback) => {
-      elem.addEventListener("click", () => {
-        luaCallback().catch(console.error);
+    set_contents: (text) => elem[['input','textarea'].includes(tag)?'value':'innerText'] = text,
+    set_href: (text) => elem.href = text,
+    set_source: (src) => elem.src = src,
+    set_opacity: (opa) => elem.style.opacity = opa,
+
+    on_click: (callback) => {
+      elem.addEventListener('click', () => {
+        callback().catch(console.error);
       });
+    },
+    on_input: (callback) => {
+      elem.addEventListener('keyup', () => {
+        callback(elem.value || elem.checked).catch(console.error);
+      });
+      elem.addEventListener('change', ()=>{
+        callback(elem.value || elem.checked).catch(console.error);
+      });
+    },
+    /*
+    on_submit: (f) => {
+        c.addEventListener(`submit`, async()=>{
+            doSneaky(f, c.value || c.checked)
+        })
+        c.addEventListener(`keyup`, async(e)=>{
+            if(e.key == "Enter") doSneaky(f, c.value || c.checked)
+        })
     }
+    */
   };
-  if (['input','textarea'].includes(tag)) {
-    base.get_contents = () => elem.value;
-    base.set_contents = (text) => elem.value = text;
+  if (bussinga) {
+    base.get_content = base.get_contents;
+    base.set_contents = (text) => elem[['input','textarea'].includes(tag)?'value':'innerHTML'] = text;
+    base.set_content = base.set_contents;
+    base.get_css_name = () => elem.className || elem.tagName;
+    base.set_value = (text) => elem.value = text,
   }
   return base;
+}
+
+function load(res, html, scripts) {
+  let doc = iframe.contentDocument;
+
+  doc.querySelector('html').innerHTML = html;
+
+  // Lua
+  for (let i = 0; i<scripts.length; i++) {
+    scripts[i] = await bussFetch(res.ip, scripts[i]);
+  }
+
+  const factory = new wasmoon.LuaFactory();
+  const lua = await factory.createEngine();
+
+  // Lua global functions
+  await lua.global.set('print', (text) => {
+    console.log(`[LUA]: ${text}`);
+    return null;
+  });
+  await lua.global.set('get', (clas, all=false) => {
+    clas = clas.trim();
+    if (all) {
+      return Array.from(doc.querySelector(clas)?doc.querySelectorAll(clas):doc.querySelectorAll('.'+clas)).map(el=>HTMLElementFunctionsFor(el));
+    } else {
+      return HTMLElementFunctionsFor(doc.querySelector(clas)??doc.querySelector('.'+clas));
+    }
+  });
+  await lua.global.set('fetch', async(o) => {
+    // TODO: add headers and body
+    let req = await fetch(o.url, {
+      method: o.method??'GET'
+    });
+    let body = await req.text();
+    try {
+      body = JSON.parse(body)
+    } catch(err) {
+      // Ignore :3
+    }
+    return body;
+  });
+  // Bussinga globals
+  if (document.getElementById('bussinga').checked) {
+    await lua.global.set('window', {
+      location: document.getElementById('url').value,
+      // TODO: What is query supposed to be
+      //query: q,
+      browser: "bussinga"
+    });
+  }
+
+  scripts.forEach(async script => {
+    script = script.replaceAll(/fetch\(\s*?\{([^¬]|¬)*?\}\s*?\)/g, function(match){return match+':await()'});
+    await lua.doString(script)
+  });
 }
 
 function view() {
@@ -45,42 +129,7 @@ function view() {
         let page = await bussFetch(res.ip, 'index.html');
         let tree = htmlparser(page);
         let build = htmlbuilder(tree);
-        let [html, scripts] = build[0];
-        iframe.contentDocument.querySelector('html').innerHTML = html;
-
-        requestAnimationFrame(async() => {
-          // Lua
-          for (let i = 0; i<scripts.length; i++) {
-            scripts[i] = await bussFetch(res.ip, scripts[i]);
-          }
-
-          const factory = new wasmoon.LuaFactory();
-          const lua = await factory.createEngine();
-
-          // Lua global functions
-          await lua.global.set('print', (text) => {
-            console.log(`[LUA]: ${text}`);
-            return null;
-          });
-          await lua.global.set('get', (clas, all=false) => {
-            if (all) {
-              return Array.from(iframe.contentDocument.querySelectorAll('.'+clas.trim())).map(el=>HTMLElementFunctionsFor(el));
-            } else {
-              return HTMLElementFunctionsFor(iframe.contentDocument.querySelector('.'+clas.trim()));
-            }
-          });
-          await lua.global.set('fetch', (o) => {
-            // TODO: Support headers and body
-            //headers = { ["Content-Type"] = "application/json" },
-            //body = '{ "test": ' .. test .. '}'
-            const xhr = new XMLHttpRequest();
-            xhr.open(o.method??'GET', o.url, false);
-            xhr.send(null);
-            return xhr.response;
-          });
-
-          scripts.forEach(async script => await lua.doString(script));
-        });
+        load(res, ...build[0])
       };
       iframe.contentDocument.location.reload();
     })
