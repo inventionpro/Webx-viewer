d6d80b2b9f5e4421a3c0c664b5f54aa960c37619
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-08-06 15:49:52 +0200

    Fixes

--- media/index.js
+++ media/index.js
@@ -1,7 +1,7 @@
 import { Browser } from './core.js'
 
-const TabContainer = document.getElementById('tabs');
-new Sortable(TabContainer.querySelector('div'), {
+const TabContainer = document.querySelector('#tabs div');
+new Sortable(TabContainer, {
   animation: 150,
   filter: '.add',
   preventOnFilter: false,
@@ -19,13 +19,13 @@ function stdout(text, type='log', tab='Browser') {
 }
 
 function showTabs() {
-  TabContainer.innerHTML = '<div>'+window.browser.tabs
+  TabContainer.innerHTML = window.browser.tabs
     .map(tab=>`<button onclick="window.browser.changeTab('${tab.id}')"${window.browser.activeTab===tab.id?' active':''} draggable="true">
   <img src="${tab.icon}" width="16" height="16">
   <span class="title">${tab.title}</span>
   <span class="close" onclick="event.stopPropagation();window.browser.closeTab('${tab.id}')">x</span>
 </button>`)
-    .join('')+`</div><button class="add" onclick="window.browser.createTab()"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 256 256"><rect x="103" width="50" height="256" rx="25"/><rect y="103" width="256" height="50" rx="25"/></svg></button>`;
+    .join('');
 }
 window.showTabs = showTabs;
 
