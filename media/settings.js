985ebfc339bc5d0d141ede177c079ed752b93625
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-08-07 15:40:38 +0200

    Better settings 2

--- media/settings.js
+++ media/settings.js
@@ -26,7 +26,7 @@ const settingsPanel = document.getElementById('settings');
 let settingsPanelOpen = false;
 function toggleSettings() {
   settingsPanelOpen = !settingsPanelOpen;
-  settingsPanel.style.width = settingsPanelOpen?'0vw':'20vw';
+  settingsPanel.style.right = settingsPanelOpen?'0px':'-20vw';
 }
 function embedPage() {
   let url = `https://inventionpro.github.io/Webx-viewer/embed?url=${document.getElementById('url').value}&bussinga=${window.Browser.bussinga}&proxy=${window.Browser.proxy}&dns=${window.Browser.dns}`;
