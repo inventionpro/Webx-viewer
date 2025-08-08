dba2fe3331e89667c78f7d36b2839bd4d742da35
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-08-08 10:23:10 +0200

    Theme improvements

--- media/settings.js
+++ media/settings.js
@@ -1,23 +1,37 @@
 // Load preferences and listen for changes
 window.setSettings = ()=>{
-  function setntrack(name, prop, def, brow=true, handle=()=>{}) {
+  function setntrack(name, prop, def) {
     const input = document.getElementById(name);
     input[prop] = localStorage.getItem(name)??def;
-    if (brow) window.browser[name] = input[prop];
-    handle(input[prop]);
+    window.browser[name] = input[prop];
     input.onchange = (evt)=>{
-      if (brow) window.browser[name] = evt.target[prop];
-      handle(evt.target[prop]);
+      window.browser[name] = evt.target[prop];
       localStorage.setItem(name, evt.target[prop]);
     };
   }
   setntrack('bussinga', 'checked', false);
   setntrack('proxy', 'checked', false);
-  setntrack('theme', 'value', '#1a1a1a', false, (val)=>{document.body.style.setProperty('--base',val)});
   setntrack('startUrl', 'value', 'buss://search.app');
   setntrack('searchUrl', 'value', 'buss://search.app?q=%1');
   setntrack('dns', 'value', 'https://dns.webxplus.org/');
+
+  const ThemeInput = document.getElementById('theme');
+  ThemeInput.value = localStorage.getItem('theme')??'#1a1a1a';
+  document.body.style.setProperty('--base', ThemeInput.value);
+  ThemeInput.oninput = (evt)=>{
+    document.body.style.setProperty('--base', evt.target.value);
+    localStorage.setItem('theme', evt.target.value);
+  };
 };
+
+Array.from(document.querySelectorAll('.themes button'))
+  .forEach(btn => {
+    btn.onclick = ()=>{
+      let theme = document.getElementById('theme');
+      theme.value = getComputedStyle(btn).getPropertyValue('--color').trim();
+      theme.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  });
 
 /*
 document.body.insertAdjacentElement('afterbegin', tabs)
