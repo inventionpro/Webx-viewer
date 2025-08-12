0526dacc7c7fd8bb6c077293186cdbe76cea8fde
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-08-12 13:20:32 +0200

    Fix

--- media/core.js
+++ media/core.js
@@ -290,6 +290,7 @@ export class Browser {
         target.splice(4, 0, 'main');
         target = target.join('/').replace(':/','://');
       }
+      if (!path.includes('.')) path += '/index.html';
     } else {
       target = new URL(('.'+path).replace(/^\.\./,'.').replace(/^\.([^\/])/,'./$1'), target+'/').href;
     }
